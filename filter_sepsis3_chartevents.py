83f0f9814de3f7384a9c6b8375b3ed04539bef98
Mattyws Grawe <mattyws@gmail.com>
2019-03-08 12:17:26 -0300

    Adding filtering on chartevents

--- filter_sepsis3_chartevents.py
+++ filter_sepsis3_chartevents.py
@@ -1,15 +1,18 @@
 import json
 import os
 import pprint
+from datetime import datetime, timedelta
 
 import functions
-#TODO : change script to chartevents
+#TODO : filtrar adimissões que a hora da infecção tenha ocorrido com no mínimo Xh após a internação do paciente
 pp = pprint.PrettyPrinter(indent=5)
-max_hours_since_admission = 12
+preciding_time_since_infection = 4
 
-sepsis3_json_files_paths  = ["/home/mattyws/Documents/json_sepsis", "/home/mattyws/Documents/json_no_sepsis"]
+sepsis3_json_files_paths  = ["/home/mattyws/Documentos/mimic/data/json_sepsis" ] #, "/home/mattyws/Documents/json_no_sepsis"]
+datetime_pattern = "%Y-%m-%d %H:%M:%S"
 
 total_files = 0
+total_patient_infection_before_adm = 0
 for sepsis3_json_files_path in sepsis3_json_files_paths:
     for dir, path, files in os.walk(sepsis3_json_files_path):
         if "no_sepsis" in dir:
@@ -28,16 +31,26 @@ for sepsis3_json_files_path in sepsis3_json_files_paths:
                     if "chartevents" in admission.keys():
                         if "no_sepsis" in dir:
                             time_to_use = admission["admittime"]
-                            filtered_noteevents = functions.filter_since_time(admission['chartevents'], time_to_use,
-                                                                              max_hours_since_admission,
-                                                                              key="charttime", after=True)
+                            filtered_chartevents = functions.filter_since_time(admission['chartevents'], time_to_use,
+                                                                               12,
+                                                                               key="charttime", after=True)
                         else:
-                            time_to_use = admission["sepsis3"]["suspected_infection_time_poe"]
-                            filtered_noteevents = functions.filter_since_time(admission['chartevents'], time_to_use,
-                                                                              max_hours_since_admission,
-                                                                              key="charttime")
-                        if len(filtered_noteevents) > 0:
-                            with open(dir_chartevents + "/" + file, 'w') as noteevents_file_handler:
-                                json.dump(filtered_noteevents, noteevents_file_handler)
-                except:
-                    print("{} failed to process".format(file))
+                            admittime = admission["admittime"]
+                            infection_time = admission["sepsis3"]["suspected_infection_time_poe"]
+                            # print(admission['hadm_id'], admittime, infection_time)
+                            # filtered_chartevents = functions.filter_events_before_infection(admission['chartevents'], admittime,
+                            #                                                                 infection_time, preciding_time_since_infection,
+                            #                                                                 time_key="charttime")
+                            admittime_datetime = datetime.strptime(admittime, datetime_pattern)
+                            infection_datetime = datetime.strptime(infection_time, datetime_pattern) - timedelta(
+                                hours=preciding_time_since_infection)
+                            if infection_datetime < admittime_datetime :
+                                total_patient_infection_before_adm += 1
+                        # for event in filtered_chartevents:
+                        #     print(event['charttime'])
+                        # if len(filtered_noteevents) > 0:
+                        #     with open(dir_chartevents + "/" + file, 'w') as noteevents_file_handler:
+                        #         json.dump(filtered_noteevents, noteevents_file_handler)
+                except Exception as e:
+                    print("{} failed to process".format(file), e)
+print(total_patient_infection_before_adm)
--- functions.py
+++ functions.py
@@ -1,16 +1,13 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import time
 
 
 DATE_PATTERN = "%Y-%m-%d"
 DATETIME_PATTERN = "%Y-%m-%d %H:%M:%S"
 
-#TODO : Change to filter since admission time until infection event
-"""
 
-"""
 def filter_events_before_infection(events, admittime, infection_time, preceding_time,
-                                   datetime_pattern=DATETIME_PATTERN, time_key="charttime")
+                                   datetime_pattern=DATETIME_PATTERN, time_key="charttime"):
     """
     Get events that occur from admission time until infection time minus preceding time
     :param events: the events
@@ -22,7 +19,15 @@ def filter_events_before_infection(events, admittime, infection_time, preceding_
     :return: 
     """
     admittime_datetime = datetime.strptime(admittime, datetime_pattern)
-    infection_datetime = datetime.strptime(admittime, datetime_pattern)
+    infection_datetime = datetime.strptime(infection_time, datetime_pattern) - timedelta(hours=preceding_time)
+    new_events = []
+    for event in events:
+        # Pega a data do evento e o transforma em datetime
+        event_datetime = datetime.strptime(event[time_key], datetime_pattern)
+        # Compara se o evento aconteceu entre a data de adimissão e a data de infecção (já alterada)
+        if event_datetime > admittime_datetime and event_datetime <= infection_datetime:
+            new_events.append(event)
+    return new_events
 
 
 
