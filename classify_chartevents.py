Fix training and normalization errors

classify_chartevents.py
@@ -50,7 +50,7 @@ data_csv = data_csv[data_csv['icustay_id'].isin(data)]
 data = np.array([parameters['dataPath'] + '{}.csv'.format(itemid) for itemid in data])
 print("========= Transforming classes")
 print(data_csv['class'].value_counts())
-classes = np.array([1 if c == 'sepsis' else 1 for c in list(data_csv['class'])])
+classes = np.array([0 if c == 'sepsis' else 1 for c in list(data_csv['class'])])
 counts = Counter(classes)
 print(counts)
 exit()