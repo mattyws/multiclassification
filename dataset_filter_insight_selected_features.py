fe877614ba5d4bb48d429a3d6ccf3309fd91972b
mattyws <mattyws@gmail.com>
2019-08-18 22:29:27 -0300

    Solving training and normalization errors

---
+++ dataset_filter_insight_selected_features.py
@@ -0,0 +1,76 @@
+"""
+Filter selected features ids in mimic dataset and gets statistical values about the filtered features
+"""
+import pandas as pd
+import numpy as np
+
+import functions
+import os
+
+def filter_features(files_list, events_ids, manager_queue=None):
+    # TODO 2: paralel processes
+    all_ids = []
+    for key in events_ids.keys():
+        all_ids.extend(events_ids[key])
+    data_statistic = dict()
+    for f in files_list:
+        patient_events = pd.read_csv(f)
+        ids_in_patients = ['Unnamed: 0']
+        for column in patient_events.columns:
+            if column != 'Unnamed: 0' and int(column) in all_ids:
+                ids_in_patients.append(column)
+        patient_events = patient_events[ids_in_patients]
+        patient_events.loc[:, 'Unnamed: 0'] = pd.to_datetime(patient_events['Unnamed: 0'],
+                                                             format=parameters['datetime_pattern'])
+        patient_events = patient_events.set_index(['Unnamed: 0']).sort_index()
+        # Transforming and getting the meaning of equal features with different ids
+        new_features = dict()
+        for key in events_ids.keys():
+            if key not in data_statistic.keys():
+                data_statistic[key] = dict()
+                data_statistic[key]["missing_patients"] = 0
+                data_statistic[key]["missing_events"] = 0
+            new_features[key] = patient_events[
+                [x for x in ids_in_patients if x != 'Unnamed: 0' and int(x) in events_ids[key]]]
+            if new_features[key].empty:
+                new_features[key] = pd.Series(np.nan, index=patient_events.index)
+                data_statistic[key]["missing_patients"] += 1
+                data_statistic[key]["missing_events"] += len(new_features[key])
+            else:
+                new_features[key] = new_features[key].mean(axis=1, skipna=True)
+                data_statistic[key]["missing_events"] += len([x for x in new_features[key].isna() if x is True])
+        patient_events = pd.DataFrame(new_features)
+
+        patient_events['pulse_pressure'] = patient_events['systolic_blood_pressure'] \
+            .sub(patient_events['diastolic_blood_pressure'], fill_value=0)
+        patient_events['gcs'] = patient_events['gcs_motor'] + patient_events['gcs_eyes'] + patient_events['gcs_verbal']
+        patient_events = patient_events.drop(
+            columns=["gcs_verbal", "gcs_motor", "gcs_eyes", "diastolic_blood_pressure"])
+        patient_events.to_csv(dataset_filtered_files_path + os.path.basename(f))
+        if manager_queue is not None:
+            manager_queue.put(f)
+    return data_statistic
+
+
+parameters = functions.load_parameters_file()
+
+dataset_merged_files_path = parameters['mimic_data_path'] + 'sepsis_chartevents/'
+dataset_filtered_files_path = parameters['mimic_data_path'] + parameters['raw_events_dirname'].format('filtered')
+
+events_ids = {
+    "systolic_blood_pressure" : [6, 51, 442, 455, 3315, 3317, 3321, 3323, 6701, 224167, 227243, 220050, 220179, 225309],
+    "diastolic_blood_pressure" : [5364, 8368, 8440, 8441, 8503, 8504, 8506, 8507, 8555, 227242, 224643, 220051, 220180,
+                                  225310],
+    "temperature_fahrenheit" : [676, 677, 3655, 226329, 223762],
+    "temperature_celcius" : [678, 679, 3652, 3654, 223761],
+    "respiratory_rate" : [614, 615, 618, 619, 653, 1884, 8113, 3603, 224688, 224689, 224690, 220210],
+    "glucose": [1445, 1310, 807, 811, 3744, 3745, 1529, 2338, 2416, 228388, 225664, 220621, 226537, 5431],
+    "heart_rate": [211, 220045],
+    "blood_oxygen_saturation": [50817],
+    "gcs_verbal" : [723, 223900],
+    "gcs_motor" : [454, 223901],
+    "gcs_eyes" : [184, 220739]
+}
+
+files_list = [dataset_merged_files_path + x for x in os.listdir(dataset_merged_files_path)]
+files_list = np.array_split(files_list, 10)
