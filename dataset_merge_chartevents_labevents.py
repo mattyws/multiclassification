66a15ab4bd9070ac265e79b10ed5f480e5574e20
mattyws@gmail.com <Kallaguer0031>
2019-08-21 14:20:23 -0300

    Change filtering insight features to multiprocessing

--- dataset_merge_chartevents_labevents.py
+++ dataset_merge_chartevents_labevents.py
@@ -31,33 +31,31 @@ dataset_csv = pd.read_csv(parameters['dataset_file_name'])
 
 def merge_events(icustay_id, chartevents_files_path, labevents_files_path, new_events_files_path, datetime_pattern):
     if not os.path.exists(new_events_files_path+'{}.csv'.format(icustay_id)) :
-        if os.path.exists(chartevents_files_path + '{}.csv'.format(icustay_id)) or \
-                os.path.exists(labevents_files_path + '{}.csv'.format(icustay_id)):
-            print("#### {} ####".format(icustay_id))
-            if os.path.exists(chartevents_files_path + '{}.csv'.format(icustay_id)):
-                chartevents = pd.read_csv(chartevents_files_path + '{}.csv'.format(icustay_id))
-                chartevents.loc[:, 'Unnamed: 0'] = pd.to_datetime(chartevents['Unnamed: 0'], format=datetime_pattern)
-                chartevents = chartevents.set_index(["Unnamed: 0"])
-            else:
-                chartevents = None
-            if os.path.exists(labevents_files_path + '{}.csv'.format(icustay_id)):
-                labevents = pd.read_csv(labevents_files_path + '{}.csv'.format(icustay_id))
-                labevents.loc[:, 'Unnamed: 0'] = pd.to_datetime(labevents['Unnamed: 0'], format=datetime_pattern)
-                labevents = labevents.set_index(["Unnamed: 0"])
-            else:
-                labevents = None
-            if chartevents is None:
-                chartevents = pd.DataFrame([])
-            if labevents is None:
-                labevents = pd.DataFrame([])
-            chartevents = chartevents.add_prefix('chartevents_')
-            labevents = labevents.add_prefix('labevents_')
-            events = pd.merge(chartevents, labevents, how='outer', left_index=True, right_index=True)
-            if 'labevents_Unnamed: 0' in events.columns:
-                events = events.drop(columns=['labevents_Unnamed: 0'])
-            if 'chartevents_Unnamed: 0' in events.columns:
-                events = events.drop(columns=['chartevents_Unnamed: 0'])
-            events.to_csv(new_events_files_path + '{}.csv'.format(icustay_id), quoting=csv.QUOTE_NONNUMERIC)
+        print("#### {} ####".format(icustay_id))
+        if os.path.exists(chartevents_files_path + '{}.csv'.format(icustay_id)):
+            chartevents = pd.read_csv(chartevents_files_path + '{}.csv'.format(icustay_id))
+            chartevents.loc[:, 'Unnamed: 0'] = pd.to_datetime(chartevents['Unnamed: 0'], format=datetime_pattern)
+            chartevents = chartevents.set_index(["Unnamed: 0"])
+        else:
+            chartevents = None
+        if os.path.exists(labevents_files_path + '{}.csv'.format(icustay_id)):
+            labevents = pd.read_csv(labevents_files_path + '{}.csv'.format(icustay_id))
+            labevents.loc[:, 'Unnamed: 0'] = pd.to_datetime(labevents['Unnamed: 0'], format=datetime_pattern)
+            labevents = labevents.set_index(["Unnamed: 0"])
+        else:
+            labevents = None
+        if chartevents is None:
+            chartevents = pd.DataFrame([])
+        if labevents is None:
+            labevents = pd.DataFrame([])
+        chartevents = chartevents.add_prefix('chartevents_')
+        labevents = labevents.add_prefix('labevents_')
+        events = pd.merge(chartevents, labevents, how='outer', left_index=True, right_index=True)
+        if 'labevents_Unnamed: 0' in events.columns:
+            events = events.drop(columns=['labevents_Unnamed: 0'])
+        if 'chartevents_Unnamed: 0' in events.columns:
+            events = events.drop(columns=['chartevents_Unnamed: 0'])
+        events.to_csv(new_events_files_path + '{}.csv'.format(icustay_id), quoting=csv.QUOTE_NONNUMERIC)
 
 # Using as arg only the icustay_id, bc of fixating the others parameters
 args = list(dataset_csv['icustay_id'])
