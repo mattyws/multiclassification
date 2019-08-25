ef964b79a15d47cfc7e233478fe3d042820eac2a
mattyws <mattyws@gmail.com>
2019-08-25 14:50:46 -0300

    Solving training and normalization errors

--- classification.py
+++ classification.py
@@ -125,7 +125,7 @@ with open(parameters['resultFilePath'], 'a+') as cvsFileHandler: # where the res
                                                                           , classes_filename=parameters['training_events_sizes_labels_file'])
             test_sizes, test_labels = functions.divide_by_events_lenght(normalized_data[testIndex], classes[testIndex]
                                                                 , sizes_filename = parameters['testing_events_sizes_file']
-                                                                , classes_filename = parameters['tessting_events_sizes_labels_file'])
+                                                                , classes_filename = parameters['testing_events_sizes_labels_file'])
             dataTrainGenerator = LengthLongitudinalDataGenerator(train_sizes, train_labels)
             dataTestGenerator = LengthLongitudinalDataGenerator(test_sizes, test_labels)
             # dataTrainGenerator = LongitudinalDataGenerator(normalized_data[trainIndex],
