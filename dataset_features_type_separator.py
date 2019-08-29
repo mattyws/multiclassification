9624f818cef99aee0b7ea2527120dfdc1f58f4c3
mattyws@gmail.com <Kallaguer0031>
2019-08-29 15:48:01 -0300

    Classification will not resume betwee

--- dataset_features_type_separator.py
+++ dataset_features_type_separator.py
@@ -9,6 +9,10 @@ import sys
 
 import functions
 import pandas as pd
+import numpy as np
+
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 3)
 
 parameters = functions.load_parameters_file()
 pp = pprint.PrettyPrinter(indent=4)
@@ -65,21 +69,28 @@ for key in features_types.keys():
         features_to_process[key] = features_types[key]
 
 i = 0
+new_events_path = parameters['mimic_data_path'] + parameters['separated_features_types_dirname']
+if not os.path.exists(new_events_path):
+    os.mkdir(new_events_path)
 for icustay in dataset['icustay_id']:
     sys.stderr.write('\rdone {0:%}'.format(i / len(dataset)))
     if not os.path.exists(patient_events_path + "{}.csv".format(icustay)):
         continue
     events = pd.read_csv(patient_events_path + "{}.csv".format(icustay))
+    features_to_drop = []
     for feature in features_to_process.keys():
         if feature in events.columns:
-
+            aux = events[feature].copy()
+            events[feature + '_numeric'] = pd.to_numeric(events[feature], errors='coerce')
+            events[feature + '_categorical'] = np.where(events[feature+'_numeric'].isna(), aux, np.nan)
+            features_to_drop.append(feature)
+            if events[feature + '_numeric'].dropna().empty:
+                features_to_drop.append(feature + '_numeric')
+            if events[feature + '_categorical'].dropna().empty:
+                features_to_drop.append(feature + '_categorical')
+    events = events.drop(columns=features_to_drop)
+    events = events.sort_index(axis=1)
+    events.to_csv(new_events_path + "{}.csv".format(icustay))
+    if i == 10:
+        exit()
     i+= 1
-
-
-
-# pp.pprint(features_types)
-formated_values = pp.pformat(values_for_mixed_in_patient)
-file = open('aux', 'w')
-file.write(formated_values)
-file.close()
-print(len(values_for_mixed_in_patient.keys()), values_for_mixed_in_patient.keys())
---
+++ dataset_remove_low_frequency_features.py
@@ -0,0 +1,58 @@
+"""
+Remove features that have a low frequency of occurrence in patients.
+"""
+
+import os
+import pickle
+import pprint
+
+import sys
+
+import functions
+import pandas as pd
+import numpy as np
+
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 3)
+
+parameters = functions.load_parameters_file()
+pp = pprint.PrettyPrinter(indent=4)
+
+dataset = pd.read_csv(parameters['mimic_data_path'] + parameters['insight_dataset_file_name'])
+patient_events_path = parameters['mimic_data_path'] + "sepsis_separated_features/"
+
+features_frequency = dict()
+i = 0
+for icustay in dataset['icustay_id']:
+    sys.stderr.write('\rdone {0:%}'.format(i / len(dataset)))
+    if not os.path.exists(patient_events_path + "{}.csv".format(icustay)):
+        continue
+    events = pd.read_csv(patient_events_path + "{}.csv".format(icustay))
+    for feature in events.columns:
+        if feature not in features_frequency.keys():
+            features_frequency[feature] = 0
+        features_frequency[feature] += 1
+    i += 1
+
+features_to_remove = set()
+for feature in features_frequency.keys():
+    if features_frequency[feature] / len(dataset) < 0.20:
+        features_to_remove.add(feature)
+
+
+i = 0
+new_events_path = parameters['mimic_data_path'] + parameters['features_low_frequency_removed_dirname']
+if not os.path.exists(new_events_path):
+    os.mkdir(new_events_path)
+
+for icustay in dataset['icustay_id']:
+    sys.stderr.write('\rdone {0:%}'.format(i / len(dataset)))
+    if not os.path.exists(patient_events_path + "{}.csv".format(icustay)):
+        continue
+    events = pd.read_csv(patient_events_path + "{}.csv".format(icustay))
+    features_in_patient = set(events.columns)
+    features_to_remove_from_patient = list(features_to_remove.intersection(features_in_patient))
+    events = events.drop(columns=features_to_remove_from_patient)
+    events = events.sort_index(axis=1)
+    events.to_csv()
+    i += 1
