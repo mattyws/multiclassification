574d2dac139ec178bb9787d89151e08becaba77f
mattyws@gmail.com <Kallaguer0031>
2019-09-19 15:03:34 -0300

    Classification will not resume betwee

--- dataset_notes_tokenize_and_remove.py
+++ dataset_notes_tokenize_and_remove.py
@@ -3,6 +3,9 @@ from functools import partial
 
 import numpy
 import pandas as pd
+import multiprocessing as mp
+
+import sys
 from nltk import WhitespaceTokenizer
 
 import functions
@@ -14,6 +17,8 @@ def remove_only_special_characters_tokens(tokens):
     for token in tokens:
         if not re.match(r'^[_\W]+$', token):
             new_tokens.append(token)
+        else:
+            print(token)
     return new_tokens
 
 
@@ -25,24 +30,37 @@ def process_notes(icustays, noteevents_data_path=None, tokenized_events_data_pat
         if not os.path.exists(noteevents_data_path + "{}.csv".format(icustay)):
             continue
         patient_noteevents = pd.read_csv(noteevents_data_path + "{}.csv".format(icustay))
+        patient_tokenized_noteevents = pd.DataFrame([])
         for index, row in patient_noteevents.iterrows():
+            print(row['Note'])
             tokens = tokenizer.tokenize(row['Note'])
             tokens = [word.lower() for word in tokens]
             tokens = remove_only_special_characters_tokens(tokens)
-            #TODO create new csv file with new tokens
-    return tokens_features
+            print(tokens)
+            row['Note'] = tokens
+            patient_tokenized_noteevents = patient_tokenized_noteevents.append(row)
+            print(patient_tokenized_noteevents)
+        patient_tokenized_noteevents.to_csv(tokenized_events_data_path + "{}.csv".format(icustay))
+        exit()
 
 parameters = functions.load_parameters_file()
 dataset = pd.read_csv(parameters['mimic_data_path'] + parameters['dataset_file_name'])
 noteevents_data_path = parameters['mimic_data_path'] + "sepsis_noteevents/"
 icustays = dataset['icustay_id'].tolist()
 icustays = numpy.array_split(icustays, 10)
+if not os.path.exists(parameters['mimic_data_path'] + parameters['tokenized_noteevents_dirname']):
+    os.mkdir(parameters['mimic_data_path'] + parameters['tokenized_noteevents_dirname'])
 
 with mp.Pool(processes=4) as pool:
     m = mp.Manager()
     queue = m.Queue()
     partial_process_notes = partial(process_notes,
+                                    noteevents_data_path=noteevents_data_path,
+                                    tokenized_events_data_path=parameters['mimic_data_path']
+                                                               + parameters['tokenized_noteevents_dirname'],
                                     manager_queue=queue)
+    partial_process_notes(icustays[0])
+    exit()
     print("===== Processing events =====")
     map_obj = pool.map_async(partial_process_notes, icustays)
     consumed = 0
@@ -52,11 +70,3 @@ with mp.Pool(processes=4) as pool:
             consumed += 1
         sys.stderr.write('\rdone {0:%}'.format(consumed / len(dataset)))
     results = map_obj.get()
-    tokens_features = create_features_dict()
-    print("===== Merging results =====")
-    for result in results:
-        tokens_features = merge_features_dictionaries(tokens_features, result)
-
-    with open(parameters['mimic_data_path'] + parameters['notes_tokens_features_file_name'], 'wb') as file:
-        pickle.dump(tokens_features, file)
-    print(tokens_features['total_tokens'])
