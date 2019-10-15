b03793a52685e4bd99170b4f058ef09bb51f121b
mattyws@gmail.com <mattyws@gmail.com>
2019-10-14 23:07:03 -0300

    Changing on low frequency features removal

--- dataset_check_features_from_directory_files.py
+++ dataset_check_features_from_directory_files.py
@@ -2,6 +2,8 @@ import os
 import pandas as pd
 import pprint
 
+import sys
+
 import functions
 
 parameters = functions.load_parameters_file()
@@ -10,12 +12,16 @@ directory = parameters['mimic_data_path'] + "sepsis_low_frequency_removed/"
 pp = pprint.PrettyPrinter(indent=4)
 features_lens = dict()
 features = None
-for file in os.listdir(directory):
+listdir = os.listdir(directory)
+consumed = 0
+for file in listdir:
     events = pd.read_csv(directory + file)
     if features is None:
         features = set(events.columns)
     if len(events.columns) not in features_lens.keys():
         features_lens[len(events.columns)] = 0
     features_lens[len(events.columns)] += 1
+    consumed += 1
+    sys.stderr.write('\rdone {0:%}'.format(consumed / len(listdir)))
 
 pp.pprint(features_lens)
