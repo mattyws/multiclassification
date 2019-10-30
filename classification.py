bf624324ce1b21fb6520b4e78155d89473ec9615
mattyws@gmail.com <Kallaguer0031>
2019-10-30 13:47:30 -0300

    Changing classification with noteevents script

--- classification.py
+++ classification.py
@@ -136,7 +136,8 @@ with open(parameters['resultFilePath'], 'a+') as cvsFileHandler: # where the res
             return size
 
         for i in range(0, len(dataTrainGenerator)):
-            print(getsize(dataTrainGenerator[i]))
+            size = getsize(dataTrainGenerator[i]) / (1024 * 1024)
+            print(size)
         # print(dataTrainGenerator[0][1])
         # print(dataTrainGenerator[0][0].shape)
         # print(dataTrainGenerator[0][1].shape)
