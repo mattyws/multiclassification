3ba61105c67b4060f918ac542ac319faae1c8441
mattyws@gmail.com <Kallaguer0031>
2019-12-12 14:33:36 -0300

    testing script on text classification

--- adapter.py
+++ adapter.py
@@ -48,9 +48,7 @@ class KerasGeneratorAdapter(ModelAdapter):
         #                          callbacks=callbacks, use_multiprocessing=True)
         for i in range(len(dataGenerator)):
             data = dataGenerator[i]
-            self.model.train_on_batch(data[0], data[1], epochs=epochs, initial_epoch=0, max_queue_size=1, verbose=1,
-                                 workers=workers, validation_data=validationDataGenerator, validation_steps=validationSteps,
-                                 callbacks=callbacks, use_multiprocessing=True)
+            self.model.train_on_batch(data[0], data[1])
 
     def predict(self, testDocs, batch_size=10):
         # result = self.model.predict(testDocs, batch_size, verbose=0)
