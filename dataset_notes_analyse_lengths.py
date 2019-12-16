29b5ccfb384451f80ef9a45ee9eedb4f06a26e17
mattyws@gmail.com <Kallaguer0031>
2019-12-16 11:02:42 -0300

    testing script on text classification

--- dataset_notes_analyse_lengths.py
+++ dataset_notes_analyse_lengths.py
@@ -13,9 +13,11 @@ import multiprocessing as mp
 import sys
 
 import functions
+from functions import escape_invalid_xml_characters, escape_html_special_entities, text_to_lower, \
+    remove_sepsis_mentions, remove_only_special_characters_tokens, whitespace_tokenize_text
 
 
-def get_sizes(patients, events_path=None, manager_queue=None):
+def get_sizes(patients, events_path=None, preprocessing_pipeline=None, manager_queue=None):
     sequences_sizes = []
     texts_sizes = []
     for index, patient in patients.iterrows():
@@ -26,7 +28,11 @@ def get_sizes(patients, events_path=None, manager_queue=None):
         patient_events = pd.read_csv(events_path + "{}.csv".format(patient['icustay_id']))
         sequences_sizes.append(len(patient_events))
         for event_index, event in patient_events.iterrows():
-            texts_sizes.append(len(event['Note']))
+            note = event['Note']
+            if preprocessing_pipeline is not None:
+                for f in preprocessing_pipeline:
+                    note = f(note)
+            texts_sizes.append(len(note))
     return sequences_sizes, texts_sizes
 
 
@@ -34,6 +40,8 @@ parameters = functions.load_parameters_file()
 
 dataset = pd.read_csv(parameters['mimic_data_path'] + parameters['dataset_file_name'])
 dataset_for_mp = numpy.array_split(dataset, 10)
+preprocessing_pipeline = [escape_invalid_xml_characters, escape_html_special_entities, text_to_lower,
+                          whitespace_tokenize_text, remove_only_special_characters_tokens, remove_sepsis_mentions]
 total_files = len(dataset)
 events_path = parameters['mimic_data_path'] + 'sepsis_noteevents/'
 sequences_sizes_file_name = parameters['mimic_data_path'] + 'sequences_sizes.pkl'
@@ -48,7 +56,8 @@ else:
     with mp.Pool(processes=6) as pool:
         m = mp.Manager()
         queue = m.Queue()
-        partial_get_sizes = partial(get_sizes, events_path=events_path, manager_queue=queue)
+        partial_get_sizes = partial(get_sizes, events_path=events_path, preprocessing_pipeline=preprocessing_pipeline,
+                                    manager_queue=queue)
         map_obj = pool.map_async(partial_get_sizes, dataset_for_mp)
         consumed = 0
         while not map_obj.ready():
