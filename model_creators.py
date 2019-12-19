38e2cb55a459184fe02cc735b2062a6903533f7e
mattyws@gmail.com <Kallaguer0031>
2019-12-19 11:54:15 -0300

    testing script on text classification

--- model_creators.py
+++ model_creators.py
@@ -64,9 +64,8 @@ class NoteeventsClassificationModelCreator(ModelCreator):
     def build_network(self):
         representation_model = Sequential()
         representation_model.add(Masking(mask_value=0.))
-        representation_model.add(LSTM(192, activation='relu'))
-        representation_model.add(Dropout(128))
-        representation_model.add(Dense(128))
+        representation_model.add(LSTM(64, activation='relu', dropout=.3))
+        representation_model.add(Dense(32))
         # cnn_model.add(AveragePooling1D(pool_size=1))
         # representation_model.add(GlobalAveragePooling1D())
 
